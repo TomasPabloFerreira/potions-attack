94dc3a532d074671a206084cd219db92122d0939
TomasPabloFerreira <tomasferreira373@gmail.com>
2021-04-13 02:28:51 -0300

    styles: app background

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<LinearGradient
+			colors={['#667eea', '#764ba2']}
+			style={styles.background}
+		>
+			<Text>Potions Attack</Text>
+			<StatusBar style="auto" />
+		</LinearGradient>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	background: {
+		flex: 1,
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		paddingTop: '40%',
+		paddingBottom: '20%'
+	}
+})
