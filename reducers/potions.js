d9ba9a9d1aba83245f128b1bd2dc0564df48e9a2
TomasPabloFerreira <tomasferreira373@gmail.com>
2021-04-13 02:20:42 -0300

    feat: create potions reducer

--- reducers/potions.js
+++ reducers/potions.js
@@ -1,4 +1,18 @@
 const reducer = (state, action) => {
+	const color = action.payload.color
+	switch (action.type) {
+		case 'add':
+			return {
+					...state,
+					[color]: state[color] + 1
+			}
+		case 'remove':
+			return {
+					...state,
+					[color]: state[color] - 1
+			}
+		default: return state
+	}
 }
 
 export default reducer
